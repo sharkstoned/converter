8b7fcc9f72b6c77f7ee0835770d90a5f645408e6
Stanley Raschinkin <stanlyraschinkin@gmail.com>
2017-05-23 21:46:05 +0300

    changed form

--- Unit1.pas
+++ Unit1.pas
@@ -5,7 +5,7 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Buttons, Vcl.StdCtrls, Vcl.Mask,
-  Vcl.ComCtrls, Vcl.Samples.Spin;
+  Vcl.ComCtrls, Vcl.Samples.Spin, Vcl.Imaging.pngimage, Vcl.ExtCtrls;
 
 type
   TConverter = class(TForm)
@@ -14,6 +14,8 @@ type
     convert_button: TButton;
     first_value: TSpinEdit;
     second_value: TSpinEdit;
+    Image1: TImage;
+    Label1: TLabel;
   private
     { Private declarations }
   public
